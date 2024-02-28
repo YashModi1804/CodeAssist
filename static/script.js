8d0e02b72a3a43ca3c111a25086886298ab89310
Deepak6204 <deepaksyadav04@gmail.com>
2024-02-28 23:11:45 +0530

    major changes

--- static/script.js
+++ static/script.js
@@ -3,7 +3,7 @@ document.querySelector(".thankyou").style.display="none";
 document.addEventListener("DOMContentLoaded", function() {
   var quizContainer = document.getElementById("interface");
   var startQuizBtn = document.getElementById("start-quiz-btn");
-
+  
   startQuizBtn.addEventListener("click", function() {
     // Request fullscreen
     document.getElementById("question").style.display = "block";
@@ -12,29 +12,29 @@ document.addEventListener("DOMContentLoaded", function() {
     if (quizContainer.requestFullscreen) {
       quizContainer.requestFullscreen();
     } else if (quizContainer.mozRequestFullScreen) { /* Firefox */
-      quizContainer.mozRequestFullScreen();
-    } else if (quizContainer.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-      quizContainer.webkitRequestFullscreen();
-    } else if (quizContainer.msRequestFullscreen) { /* IE/Edge */
-      quizContainer.msRequestFullscreen();
-    }
-  });
+    quizContainer.mozRequestFullScreen();
+  } else if (quizContainer.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+  quizContainer.webkitRequestFullscreen();
+} else if (quizContainer.msRequestFullscreen) { /* IE/Edge */
+quizContainer.msRequestFullscreen();
+}
+});
 
-  // Exit fullscreen
-  document.addEventListener("fullscreenchange", function() {
-    if (!document.fullscreenElement) {
-      // Submit the quiz when exiting fullscreen
-      submitQuiz();
-    }
-  });
+// Exit fullscreen
+document.addEventListener("fullscreenchange", function() {
+  if (!document.fullscreenElement) {
+    // Submit the quiz when exiting fullscreen
+    submitQuiz();
+  }
+});
 
-  function submitQuiz() {
-    // Get user selections (example: assume user selected option 2)
-    var userSelection = 2;
-
-    // Send AJAX request to PHP script to submit the selection
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "submit_quiz.php", true);
+function submitQuiz() {
+  // Get user selections (example: assume user selected option 2)
+  var userSelection = 2;
+  
+  // Send AJAX request to PHP script to submit the selection
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "submit_quiz.php", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.onreadystatechange = function() {
       if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
@@ -45,8 +45,10 @@ document.addEventListener("DOMContentLoaded", function() {
       // document.getElementById("quiz-container").style.display = "flex";
       document.querySelector(".thankyou").style.display="flex";
       
-
+      
     };
     xhr.send("selection=" + userSelection); // Send user selection as POST data
   }
 });
+
+document.getElementById("submit_button").addEventListener("click", submitQuiz());
