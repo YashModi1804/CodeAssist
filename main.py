09c65ecc7e21358e7dff86aafad4de6547e2bde0
Deepak6204 <deepaksyadav04@gmail.com>
2024-02-28 22:58:31 +0530

    added main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+
+from flask import Flask, render_template
+
+# database
+import pymysql
+
+timeout = 10
+connection = pymysql.connect(
+    charset="utf8mb4",
+    connect_timeout=timeout,
+    cursorclass=pymysql.cursors.DictCursor,
+    db="defaultdb",
+    host="codeassist-codeassist.a.aivencloud.com",
+    password="AVNS_dIB72iujYgE0YglUFSo",
+    read_timeout=timeout,
+    port=24991,
+    user="avnadmin",
+    write_timeout=timeout,
+)
+
+try:
+    cursor = connection.cursor()
+    # cursor.execute("create table student_data (name varchar(30), enroll varchar(11) primary key, answer varchar(500));")
+    # cursor.execute("set autocommit = 1;")
+    # cursor.execute("insert into student_data values('Deepak Yadav', '2022BCSE036', 'galat ans');")
+    # cursor.execute("delete from student_data where name = 'Deepak Yadav';")
+    cursor.execute("select * from student_data;")
+    
+    print(cursor.fetchall())
+finally:
+    # print("closed")
+    connection.close()
+
+app = Flask(__name__)
+
+# Route for the main page
+@app.route("/")
+def home():
+    return render_template("main.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
