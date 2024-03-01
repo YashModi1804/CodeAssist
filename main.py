24d27a85798970e6b51d3e0a962822b855cbe6c1
Deepak6204 <deepaksyadav04@gmail.com>
2024-03-01 21:26:37 +0530

    minor changes

--- main.py
+++ main.py
@@ -38,11 +38,28 @@ app = Flask(__name__)
 def home():
     return render_template("main.html")
 
+@app.route("/getdata")
+def getdata():
+    try:
+        cursor = connection.cursor()
+        # cursor.execute("create table student_data (name varchar(30), enroll varchar(11) primary key, answer varchar(500));")
+        cursor.execute("set autocommit = 1;")
+        # cursor.execute(f"insert into student_data values('{data['name']}', '{data['enroll']}', '{data['ans']}');")
+        # cursor.execute("delete from student_data where name = 'Deepak Yadav';")
+        cursor.execute(f"select * from student_data;")
+        
+        result = cursor.fetchall()
+        return jsonify(result)
+    except Exception as e:
+        print("Error occurred:", e)
+        # Optionally, you can raise the exception to halt the program
+        raise
+
 @app.route("/write", methods=["POST","GET"])
 def write():
     data = request.json
     res = {
-        'msg': "deepak",
+        'msg': "",
         'val':0
     }
 
@@ -50,9 +67,8 @@ def write():
         cursor = connection.cursor()
         # cursor.execute("create table student_data (name varchar(30), enroll varchar(11) primary key, answer varchar(500));")
         cursor.execute("set autocommit = 1;")
-        # cursor.execute(f"insert into student_data values('{data['name']}', '{data['enroll']}', '{data['ans']}');")
-        # cursor.execute("delete from student_data where name = 'Deepak Yadav';")
         cursor.execute(f"select * from student_data where enroll='{data['enroll']}';")
+        # cursor.execute("delete from student_data where name = 'Deepak Yadav';")
         
         result = cursor.fetchall()
         print(result)
@@ -64,9 +80,14 @@ def write():
         print("Error occurred:", e)
         # Optionally, you can raise the exception to halt the program
         raise
+    
+    cursor.execute(f"insert into student_data values('{data['name']}', '{data['enroll']}', '{data['ans']}');")
+
     res['msg'] = "answer submitted successfully"
     res['val'] = 1
     return jsonify(res)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
